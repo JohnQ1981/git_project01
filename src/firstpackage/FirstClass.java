85fc4edda5a7d632cdf54d76af4ac6aaab43dfb4
John Q <ikram1981@gmail.com>
2020-10-26 21:41:44 -0500

    MySecond Commit

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 public static void main (String[] arg) {
 	
 	System.out.println("My First commit from local repo");
+	System.out.println("Our Second Commit from Local Repository");
 	
 }
 }
