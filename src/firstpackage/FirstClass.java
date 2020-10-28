ff5b0f59b59585e832d2740d0623b4cfe524a584
John Q <ikram1981@gmail.com>
2020-10-27 19:53:04 -0500

    My Test4

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,6 +6,6 @@ public static void main (String[] arg) {
 	
 	System.out.println("My First commit from local repo");
 	System.out.println("Our Second Commit from Local Repository");
-	System.out.println("Now i am on jqosimi branch-3");
+	System.out.println("Now i am on jqosimi branch-4");
 }
 }
