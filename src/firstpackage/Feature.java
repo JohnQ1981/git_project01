a12df13290d72c651f64936fa01a65d5d3318313
John Q <ikram1981@gmail.com>
2020-10-27 20:59:53 -0500

    Ftnext

--- src/firstpackage/Feature.java
+++ src/firstpackage/Feature.java
@@ -4,7 +4,7 @@ public class Feature {
 
 	public static void main(String[] args) {
 		System.out.println("My Commit from Feature Branch");
-
+		System.out.println("Feature branch next commit");
 	}
 
 }
