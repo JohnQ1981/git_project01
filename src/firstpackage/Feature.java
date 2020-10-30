4e821d73c60b2fedab26925c188c237df0aea046
John Q <ikram1981@gmail.com>
2020-10-30 14:28:41 -0500

    T

--- src/firstpackage/Feature.java
+++ src/firstpackage/Feature.java
@@ -1,10 +1,23 @@
 package firstpackage;
 
+import java.util.Scanner;
+
+
 public class Feature {
 
 	public static void main(String[] args) {
 		System.out.println("My Commit from Feature Branch");
 		System.out.println("Feature branch next commit");
+		System.out.println("This is done on remote");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Your First name");
+		String name2 = scan.next().trim();
+		System.out.println(name2);
+		for(int i =0; i<10; i++){
+		System.out.println("Hello");
+		}
+		
+		
 	}
 
 }
